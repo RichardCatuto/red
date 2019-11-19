ca367d12f8581feec0b5afe1e4d90735e112422c
RichardCatuto <57956563+RichardCatuto@users.noreply.github.com>
2019-11-19 12:36:05 -0500

    redalumnos

---
+++ administrator/components/com_akeeba/BackupEngine/serverkey.php
@@ -0,0 +1,1 @@
+<?php defined('AKEEBAENGINE') or die(); define('AKEEBA_SERVERKEY', 'M5NVHE6ahwvzBvAXtakOJnVDlZUYun/PDIfBTH+19C2kLi/ivk2l8sWsQJT1y0A08qavBRcyBCfL/1vxrvlh9A=='); ?>
---
+++ administrator/logs/error.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-07-27 03:16:34 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-07-27T03:16:34+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-07-27T05:49:43+00:00	INFO ::1	joomlafailure	No se permiten contraseñas vacías
+2018-08-01T04:54:05+00:00	INFO ::1	joomlafailure	No se permiten contraseñas vacías
+2018-08-01T04:55:29+00:00	INFO ::1	joomlafailure	No se permiten contraseñas vacías
+2018-08-01T04:59:20+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-08-01T06:16:09+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-10-06T15:38:10+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-10-06T15:38:18+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
